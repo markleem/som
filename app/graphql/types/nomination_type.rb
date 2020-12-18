020576594ca53f9877d3398e25cd2db3d89cd441
Mark Mayfield <mmayfield@mac.com>
2020-12-18 10:23:09 -0600

    Added a nominatable_type to the GraphQL NominationType

--- app/graphql/types/nomination_type.rb
+++ app/graphql/types/nomination_type.rb
@@ -5,10 +5,15 @@ module Types
     field :nomination_date, GraphQL::Types::ISO8601Date, null: true
     field :status, String, null: false
     field :team_member, Types::TeamMemberType, null: false
+    field :nominatable_type, String, null: false
     field :nominatable, Types::NominatableType, null: false
 
     def status
       object.status_type
+    end
+
+    def nominatable_type
+      object.nominatable.class.to_s
     end
   end
 end
